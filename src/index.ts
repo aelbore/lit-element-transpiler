ebf76e4041fe8ad21d38c2bd67e496a2d1b7ab19
aelbore <aelbore.sg@gmail.com>
2020-02-28 10:10:10 +0800

    add scripts, update submodules, export magic-string

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './transpiler'
 export * from './css-preprocessors'
+export * from './libs'
--- tools/link.ts
+++ tools/link.ts
@@ -1,10 +1,15 @@
 import { symlinkDir } from 'aria-build'
 
 (async function() {
-  await symlinkDir('./dist', 
-    './node_modules/lit-element-transpiler'
-  )
-  await symlinkDir('./node_modules', 
-    './packages/rollup-plugin-inline-lit-element/node_modules'
-  )
+
+  await Promise.all([
+    symlinkDir('./dist',  './node_modules/lit-element-transpiler'),
+    symlinkDir('./node_modules', 
+      './packages/rollup-plugin-inline-lit-element/node_modules'
+    ),
+    symlinkDir('./node_modules', 
+      './packages/inline-lit-element-loader/node_modules'
+    )
+  ])
+
 })()
